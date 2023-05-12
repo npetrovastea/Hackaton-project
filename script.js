983fe8b9e0adfc65e77673553b991fe5885ef47f
npetrovastea <npetrov@asteasolutions.com>
2023-05-12 18:30:29 +0300

    comic Eli

--- script.js
+++ script.js
@@ -1,116 +1,84 @@
-const NEWS_API_KEY = '7d9d45519d2846b3872d7449ee2ddcd8';
-const GNEWS_API_KEY = '95f6ce7b7f9a8f98637ecc031696f6dd';
-const searchForm = document.getElementById('search-form');
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-button');
-const localNewsButton = document.getElementById('local-news-button');
-const resultsContainer = document.getElementById('results-container');
-const newsContainer = document.getElementById('news-container');
+// Global variables to keep track of the current page and total number of pages
+let currentPage = 1;
+const numPages = 20;
 
-let userLocation = "";
+// Function to hide all pages and show the welcome screen
+function showWelcome() {
+  document.getElementById("welcome").style.display = "block";
+  document.getElementById("comic").style.display = "none";
+}
 
-// Event listener for search form submission
-searchForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  if (searchInput.value) {
-    searchNews(searchInput.value);
+// Function to hide the welcome screen and show the first page
+function startComic() {
+  document.getElementById("page" + currentPage).style.display = "none";
+  currentPage = 1;
+  document.getElementById("welcome").style.display = "none";
+  document.getElementById("comic").style.display = "block";
+  document.getElementById("page1").style.display = "block";
+}
+
+// Function to show the next page
+function nextPage() {
+  if (currentPage < numPages) {
+    currentPage++;
+    document.getElementById("page" + currentPage).style.display = "block";
+    document.getElementById("page" + (currentPage - 1)).style.display = "none";
   }
-});
-
-// Event listener for local news button click
-localNewsButton.addEventListener('click', (e) => {
-  e.preventDefault();
-  if (userLocation) {
-    searchNewsByLocation(userLocation);
-  } else {
-    getLocation();
-  }
-});
-
-// Function to get user location
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    resultsContainer.innerHTML = "<p>Geolocation is not supported by this browser.</p>";
+  // If we're on the last page, show the end screen
+  else {
+    showEnd();
   }
 }
 
-// Function to show user location
-function showPosition(position) {
-  const latitude = position.coords.latitude;
-  const longitude = position.coords.longitude;
-  userLocation = `${latitude},${longitude}`;
-  searchNewsByLocation(userLocation);
+function goToPage(page) {
+  document.getElementById("page" + page).style.display = "block";
+  document.getElementById("page" + currentPage).style.display = "none";
+  currentPage = page;
+}
+
+// Function to show the previous page
+function prevPage() {
+  if (currentPage > 1) {
+    document.getElementById("page" + currentPage).style.display = "none";
+    currentPage--;
+    document.getElementById("page" + currentPage).style.display = "block";
+  }
+}
+
+// Function to show the end screen
+function showEnd() {
+  document.getElementById("welcome").style.display = "none";
+  document.getElementById("comic").style.display = "none";
+  // Hide page 21 elements
+  document.getElementById("page20").style.display = "none";
+  // Hide all pages
+  for (let i = 1; i <= numPages - 1; i++) {
+    document.getElementById("page" + i).style.display = "none";
+  }
+  document.getElementById("end").style.display = "block";
 }
 
 
-
-// Function to search for news by keyword
-function searchNews(keyword) {
-  const url = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${NEWS_API_KEY}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      let news = '';
-      let count = 0; // initialize count variable
-      if (data.articles) {
-        data.articles.forEach((article) => {
-          const articleDate = new Date(article.publishedAt);
-          news += `
-          <div class="article">
-            <h2 class="title">${article.title}</h2>
-            <p class="description">${article.description}</p>
-            <span class="date">${articleDate.toDateString()}</span>
-            <a class="read-more" href="${article.url}" target="_blank">Read More</a>
-          </div>
-          `;
-          count++; // increment count for each article
-        });
-      } else {
-        news = '<p>No news found</p>';
-      }
-      resultsContainer.innerHTML = `
-        <h2>Results (${count})</h2>
-        ${news}
-      `; // update resultsContainer to include count
-    })
-    .catch((error) => {
-      resultsContainer.innerHTML = `<p>${error.message}</p>`;
-    });
+// Function to show the end screen
+function showEnd() {
+  // Hide page 20 elements
+  document.getElementById("page20").style.display = "none";
+  // Hide all pages
+  for (let i = 1; i <= numPages - 1; i++) {
+    document.getElementById("page" + i).style.display = "none";
+  }
+  // Hide the end screen
+  document.getElementById("end").style.display = "block";
 }
-//https://newsapi.org/v2/everything?q=location:${location}&apiKey=${NEWS_API_KEY}
-//https://newsapi.org/v2/everything?q=&apiKey=${NEWS_API_KEY}&pageSize=100&sortBy=popularity&language=en&lat=LATITUDE&lon=LONGITUDE&radius=10000`;
-// Function to search for news by location
-function searchNewsByLocation(location) {
-  const url = `https://api.gnews.io/v4/search?q='news'&location=${location}&token=${GNEWS_API_KEY}&max=100&lang=bg`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      let news = '';
-      let count = 0; // initialize count variable
-      if (data.articles) {
-        data.articles.forEach((article) => {
-          const articleDate = new Date(article.publishedAt);
-          news += `
-          <div class="article">
-            <h2 class="title">${article.title}</h2>
-            <p class="description">${article.description}</p>
-            <span class="date">${articleDate.toDateString()}</span>
-            <a class="read-more" href="${article.url}" target="_blank">Read More</a>
-          </div>
-          `;
-          count++; // increment count for each article
-        });
-      } else {
-        news = '<p>No news found</p>';
-      }
-      newsContainer.innerHTML = `
-        <h2>Results (${count})</h2>
-        ${news}
-      `; // update newsContainer to include count
-    })
-    .catch((error) => {
-      newsContainer.innerHTML = `<p>${error.message}</p>`;
-    });
+
+// Function to hide all pages and show the welcome screen
+function showWelcome() {
+  // Hide all pages
+  for (let i = 1; i <= numPages; i++) {
+    document.getElementById("page" + i).style.display = "none";
+  }
+  // Hide the end screen
+  document.getElementById("end").style.display = "none";
+  // Show the welcome screen
+  document.getElementById("welcome").style.display = "block";
 }
