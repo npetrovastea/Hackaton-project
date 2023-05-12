e77c12305dcdb48ac4374c2d3a9e075af480e5a1
npetrovastea <npetrov@asteasolutions.com>
2023-05-12 12:57:28 +0300

    new changes

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const NEWS_API_KEY = '7d9d45519d2846b3872d7449ee2ddcd8';
+const GNEWS_API_KEY = '95f6ce7b7f9a8f98637ecc031696f6dd';
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
@@ -43,13 +44,16 @@ function showPosition(position) {
   searchNewsByLocation(userLocation);
 }
 
+
+
 // Function to search for news by keyword
 function searchNews(keyword) {
-  const url = `https://gnews.io/api/v4/search?q=${keyword}&token=${NEWS_API_KEY}`;
+  const url = `https://newsapi.org/v2/everything?q=${keyword}&apiKey=${NEWS_API_KEY}`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       let news = '';
+      let count = 0; // initialize count variable
       if (data.articles) {
         data.articles.forEach((article) => {
           const articleDate = new Date(article.publishedAt);
@@ -61,24 +65,30 @@ function searchNews(keyword) {
             <a class="read-more" href="${article.url}" target="_blank">Read More</a>
           </div>
           `;
+          count++; // increment count for each article
         });
       } else {
         news = '<p>No news found</p>';
       }
-      resultsContainer.innerHTML = news;
+      resultsContainer.innerHTML = `
+        <h2>Results (${count})</h2>
+        ${news}
+      `; // update resultsContainer to include count
     })
     .catch((error) => {
       resultsContainer.innerHTML = `<p>${error.message}</p>`;
     });
 }
-
+//https://newsapi.org/v2/everything?q=location:${location}&apiKey=${NEWS_API_KEY}
+//https://newsapi.org/v2/everything?q=&apiKey=${NEWS_API_KEY}&pageSize=100&sortBy=popularity&language=en&lat=LATITUDE&lon=LONGITUDE&radius=10000`;
 // Function to search for news by location
 function searchNewsByLocation(location) {
-  const url = `https://gnews.io/api/v4/search?q=location:${location}&token=${NEWS_API_KEY}`;
+  const url = `https://api.gnews.io/v4/search?q='news'&location=${location}&token=${GNEWS_API_KEY}&max=100&lang=bg`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
       let news = '';
+      let count = 0; // initialize count variable
       if (data.articles) {
         data.articles.forEach((article) => {
           const articleDate = new Date(article.publishedAt);
@@ -90,11 +100,15 @@ function searchNewsByLocation(location) {
             <a class="read-more" href="${article.url}" target="_blank">Read More</a>
           </div>
           `;
+          count++; // increment count for each article
         });
       } else {
         news = '<p>No news found</p>';
       }
-      newsContainer.innerHTML = news;
+      newsContainer.innerHTML = `
+        <h2>Results (${count})</h2>
+        ${news}
+      `; // update newsContainer to include count
     })
     .catch((error) => {
       newsContainer.innerHTML = `<p>${error.message}</p>`;
